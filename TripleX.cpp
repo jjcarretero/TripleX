06ea33bacc1662ffb6d62a7e4da0663b522dee89
JohnShakalaka <juanjosecarreteroa@hotmail.com>
2019-11-18 18:29:02 +0100

    Final del capitulo "Hello World"

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You had been abducted!! You are at the spaceship and the aliens have you lying on a table to make experiments with your body.";
+    std::cout << std::endl;
+    std::cout << "In a mislead of your kidnappers, you have released one of your hands and punch one of those aliens in the head.";
+    std::cout << std::endl;
+    std::cout << "Taking advantage of the confusion, you have break free and run onto the door.";
+    std::cout << "There, you have to solve and strange password to keep on scaping...";
     return 0;
 }
