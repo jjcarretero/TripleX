385dc2f78542b5bfbf685372835db2c661a0efe3
JohnShakalaka <juanjosecarreteroa@hotmail.com>
2019-11-22 23:09:41 +0100

    Function Video and ASCII Art

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,15 +2,34 @@
 
 //I want to thank "DiegoTheOneEyedCat" for her help with grammar corrections and writting ideas.
 
-int main()
+void PrintIntroduction()
 {
+
+    std::cout << "====================================================================================================" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "     **     **       ** ******** ****     **   ********** *******   ** *******  **       ******** **     **" << std::endl;
+    std::cout << "    ****   /**      /**/**///// /**/**   /**  /////**/// /**////** /**/**////**/**      /**///// //**   ** " << std::endl;
+    std::cout << "   **//**  /**      /**/**      /**//**  /**      /**    /**   /** /**/**   /**/**      /**       //** **  " << std::endl;
+    std::cout << "  **  //** /**      /**/******* /** //** /**      /**    /*******  /**/******* /**      /*******   //***   " << std::endl;
+    std::cout << " **********/**      /**/**////  /**  //**/**      /**    /**///**  /**/**////  /**      /**////     **/**  " << std::endl;
+    std::cout << "/**//////**/**      /**/**      /**   //****      /**    /**  //** /**/**      /**      /**        ** //** " << std::endl;
+    std::cout << "./**     /**/********/**/********/**    //***      /**    /**   //**/**/**      /********/******** **   //**" << std::endl;
+    std::cout << "//      // //////// // //////// //      ///       //     //     // // //       //////// //////// //     // " << std::endl;
+    std::cout << std::endl;
+    std::cout << "====================================================================================================" << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
     //Print welcome messages to the terminal
-    std::cout << "You had been abducted!! You are at the spaceship and the aliens have you lying on a table to make experiments with your body.";
-    std::cout << std::endl;
-    std::cout << "In a mislead of your kidnappers, you have released one of your hands and punch one of those aliens in the head.";
-    std::cout << std::endl;
-    std::cout << "Taking advantage of the confusion, you have break free and run onto the door.";
-    std::cout << "There, you have to solve and strange password to keep on scaping..." << std::endl;
+    std::cout << "You had been abducted!! You are at the spaceship and the aliens have you lying on a table to make experiments with your body.\n";
+    std::cout << "In a mislead of your kidnappers, you have released one of your hands and punch one of those aliens in the head.\n";
+    std::cout << "Taking advantage of the confusion, you have break free and run onto the door.\n";
+    std::cout << "There, you have to solve and strange password to keep on scaping...\n\n";
+}
+
+void PlayGame()
+{
+    PrintIntroduction();
 
     //Declare first 3 number code
     const int CodeA = 4;
@@ -21,34 +40,35 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     //Print sum and product to the terminal
-    std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "\n+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
+    //Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
-    std::cout << std::endl;
 
+    //Check if the player guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You enter the three numeric values of the code and take a look back at the aliens... They are recovering!" << std::endl;
-        std::cout << "There's a metallic sound, the door opens." << std::endl;
-        std::cout << "Run! You did it!" << std::endl;
+        std::cout << "\nYou enter the three numeric values of the code and take a look back at the aliens... They are recovering!\n";
+        std::cout << "There's a metallic sound, the door opens.\n";
+        std::cout << "Run! You did it!\n";
     }
     else
     {
-        std::cout << "Your fingers enter the numbers... but it doesn't work!" << std::endl;
-        std::cout << "The aliens are getting closer each second." << std::endl;
-        std::cout << "Suddenly, a big shadow pounces on you. You are captured again, and this time... you won't be able to escape." << std::endl;
-        std::cout << std::endl;
-        std::cout << "GAME OVER" << std::endl;
+        std::cout << "\nYour fingers enter the numbers... but it doesn't work!\n";
+        std::cout << "The aliens are getting closer each second.\n";
+        std::cout << "Suddenly, a big shadow pounces on you. You are captured again, and this time... you won't be able to escape.\n\n";
+        std::cout << "GAME OVER\n";
     }
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
