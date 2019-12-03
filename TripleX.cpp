1b991cd1b8067953069fbb9236dadbd07579ecd3
JohnShakalaka <juanjosecarreteroa@hotmail.com>
2019-12-03 18:39:32 +0100

    Finished!

--- TripleX.cpp
+++ TripleX.cpp
@@ -22,38 +22,38 @@ void PrintIntroduction(int Difficulty, int PhaseCounter)
         std::cout << "===========================================================================================================" << std::endl;
 
         //Print welcome messages to the terminal
-        std::cout << "\n\nYou had been abducted!! You are at the spaceship and the aliens have you lying on a table to make experiments with your body.\n";
-        std::cout << "In a mislead of your kidnappers, you have released one of your hands and punch one of those aliens in the head.\n";
-        std::cout << "Taking advantage of the confusion, you have break free and run onto the door with a number " << Difficulty;
-        std::cout << " on top of it.\nThere, you have to solve and strange password to keep on scaping...\n\n";
+        std::cout << "\n\nYou have been abducted. You are on a spaceship, lying on a table while aliens are conducting experiments on you.\n";
+        std::cout << "You free one of your hands, taking your captors by surprise. You punch one of the aliens in the head.\n";
+        std::cout << "Taking advantage of the confusion you break free and run into a door with a number " << Difficulty;
+        std::cout << " on top of it.\nYou need to find a password to open the door and continue your escape\n\n";
     }
     else if (PhaseCounter == 1)
     {
-        std::cout << "\nAfter running through a corridor, you finally arrive to a huge metallic door\n";
-        std::cout << "It´s probably the hangar where the shuttles can be found\n";
-        std::cout << "But again, another password should be enter, and this time the door is headed with a " << Difficulty;
-        std::cout << ".\nIf you were able to take one of those ships...\n";
+        std::cout << "\nYou run through a corridor and find yourself in front of a huge metal door\n";
+        std::cout << "It must be the hangar with the shuttles\n";
+        std::cout << "Yet again, you must enter a password, and this time the door is headed with a " << Difficulty;
+        std::cout << ".\nIf only you could take one of those ships...\n";
     }
     else if (PhaseCounter == 2)
     {
-        std::cout << "\nYou are now on the pilot´s seat. You are so close to be free again!\n";
-        std::cout << "You see strange symbols on the panel control, but you already know how these codes work\n";
-        std::cout << "You just have to enter the right numbers...\n";
+        std::cout << "\nYou are in the pilot seat. You are so close to freedom!\n";
+        std::cout << "You see the strange symbols on the control panel, but you already know how these codes work\n";
+        std::cout << "You just need to enter the right numbers...\n";
     }
 
     else if (PhaseCounter == 3)
     {
-        std::cout << "\nOh! You were so close, but for some reason the engine stopped working. All you room is now shining in blue.\n";
-        std::cout << "That code was right, you were sure, but... What is happening? It's like if they were using... \n";
-        std::cout << "Yes. A sound on the board confirmed it, the aliens are using a trap ray!\n";
-        std::cout << "You must hack the computer before it's too late!\n";
+        std::cout << "\nOh! You were so close but for some reason the engine has stopped working. The room is glowing blue.\n";
+        std::cout << "You entered the right code, you are sure of it, but... what’s happening? It’s like they are using... \n";
+        std::cout << "Yes, you hear the sound confirming your suspicions. The aliens are using a trap ray!\n";
+        std::cout << "You must hack the computer before it’s too late!\n";
     }
 
     else if (PhaseCounter == 4)
     {
-        std::cout << "\nOkey, WHAT? You are free from the ray, but you are still on trouble.\n";
-        std::cout << "In the rush, you didn't notice that this ship had an anti-theft mecanism, and now you have a countdown on the monitor\n";
-        std::cout << "If you don't enter the right numbers very quickly, you will blow up!!!\n";
+        std::cout << "\nOK, what? You are free from the ray but you are still in trouble\n";
+        std::cout << "In the rush to escape you did not notice that the ship was using an anti-theft mechanism. A countdown appears on the monitor\n";
+        std::cout << "If you don’t enter the right numbers immediately, the ship will blow up!!!\n";
         std::cout << "Come on! Do it! NOW!\n";
     }
 }
@@ -62,27 +62,27 @@ void PrintSuccess(int PhaseCounter)
 {
     if (PhaseCounter == 0)
     {
-        std::cout << "\nYou enter the three numeric values of the code and take a look back at the aliens... They are recovering!\n";
-        std::cout << "There's a metallic sound, the door opens.\n";
+        std::cout << "\nYou enter the three numeric values of the code and take a look back at the aliens... They are coming!\n";
+        std::cout << "There's a metallic sound and the door opens.\n";
         std::cout << "Run! You did it!\n";
     }
     else if (PhaseCounter == 1)
     {
-        std::cout << "\nYou enter the third number and wait for the right *beep* that confirm you are right\n";
-        std::cout << "A phaser ray makes a smoking hole near your head...\n";
-        std::cout << "But the doors opens! You are now at the hangar, and you see a nice white ship\n";
-        std::cout << "The ramps is down and you are able to get inside it. You are very close to make your way out of there!\n";
+        std::cout << "\nYou enter the third number and wait for the sound *beep* confirming it’s correct\n";
+        std::cout << "A phase ray makes a smoking hole next to your head...\n";
+        std::cout << "Finally, the door opens! You are now in the hangar and you see a nice, white ship\n";
+        std::cout << "The ramp is down and you are able to get inside of it. You are so close to making it out of here!\n";
     }
     else if (PhaseCounter == 2)
     {
-        std::cout << "\nWith a big roar the engine wakes up and start glowing\n";
-        std::cout << "You feel how the ship lifts from the floor and you breathe deeply while pulling the joystic down to make it fly\n";
+        std::cout << "\nWith a loud roar, the engine comes to life and starts glowing\n";
+        std::cout << "You feel the ship lift off and you breathe in deep as you pull the controls down to make it fly\n";
         std::cout << "Almost there...\n";
     }
 
     else if (PhaseCounter == 3)
     {
-        std::cout << "\nAfter entering the security code, the blue light stops blinking and it goes back to the previous red\n";
+        std::cout << "\nUpon entering the security code, the blue light stops flashing and goes back to red\n";
         std::cout << "You are free from the ray!\n";
         std::cout << "Let's try again!\n";
     }
@@ -90,8 +90,8 @@ void PrintSuccess(int PhaseCounter)
     else if (PhaseCounter == 4)
     {
         std::cout << "\n5...4...3...2...\n";
-        std::cout << "You close your eyes and press the last number...\n";
-        std::cout << "Hurray! The countdown stopped!!\n";
+        std::cout << "You close your eyes and enter the final number...\n";
+        std::cout << "Hurray! The countdown has stopped!!\n";
     }
 }
 
@@ -99,22 +99,22 @@ void PrintFail(int Lifes)
 {
     if (Lifes == 1)
     {
-        std::cout << "\nThe last number shines in red again. You had failed again. You look back to check your pursuers and try to get focus.\n";
-        std::cout << "Suddenly, a big shadow pounces on you. You are captured again, and this time... you won't be able to escape.\n\n";
+        std::cout << "\nThe final number flashes red once more. You have failed again. You look back at your pursuers and try to focus.\n";
+        std::cout << "Suddenly, a big shadow pounces on you. You are captured again, and this time… you won’t be able to escape.\n\n";
         std::cout << "GAME OVER\n";
         std::cout << "Thank you for playing Alien TripleX by JJCA\n";
     }
     else if (Lifes == 2)
     {
-        std::cout << "\nOh no! You failed again!\n";
-        std::cout << "Steps are closer, aliens are almost there\n";
-        std::cout << "Quickly, try again! But don't get wrong this time!\n";
+        std::cout << "\nOh no! You’ve failed again!\n";
+        std::cout << "Steps are coming closer, the aliens are almost there\n";
+        std::cout << "Quick, try it again! But don’t get it wrong this time!\n";
     }
     else if (Lifes == 3)
     {
-        std::cout << "\n*Bzzt* The panel shines in red, you were wrong!\n";
-        std::cout << "Fortunately, you had some time, you may try again\n";
-        std::cout << "Think wisely and press those numbers correctly\n";
+        std::cout << "\n*Bzzt* The panel glows red, you were wrong!\n";
+        std::cout << "Fortunately, there is still time, you can try again\n";
+        std::cout << "Think hard, try to press the numbers in the right order\n";
     }
 }
 
@@ -191,9 +191,9 @@ int main()
 
     if (Lifes != 0)
     {
-        std::cout << "\nThe ship finally speed up to leave that nightmare behind\n";
-        std::cout << "You have espcaped from the aliens, and now you have an awesome tale to tell to your grandchildren...\n";
-        std::cout << "If they ever believe you!\n\n";
+        std::cout << "\nThe ship finally speeds up and you leave the nightmare behind\n";
+        std::cout << "You managed to escape the aliens and now you have an awesome tale to tell to your grandchildren...\n";
+        std::cout << "If they believe you!\n\n";
         std::cout << "Congratulations and thank you for playing Alien TripleX by JJCA\n";
     }
     return 0;
